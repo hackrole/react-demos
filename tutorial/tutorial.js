dd06c8b9cf2970e99113e77dc833ec6ebece10fb
hackrole <daipeng123456@gmail.com>
2016-02-14 11:22:29 +0800

    step2. tutoral2 and tutorial3

--- tutorial/tutorial.js
+++ tutorial/tutorial.js
@@ -1,11 +1,34 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+
+var CommentList = React.createClass({
+  render: function(){
+    return (
+      <div className='commentList'>
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function(){
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
 var CommentBox = React.createClass({
   render: function(){
     return (
       <div classname="commentBox">
-        Hello, world! I am a CommentBox.
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
       </div>
     );
   }
