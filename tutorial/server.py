7425de91c1810e98fd85579454ab4fe8791439f1
hackrole <daipeng123456@gmail.com>
2016-02-14 12:59:51 +0800

    step5. tutorial 11/12/13/14. fetch data from server

---
+++ tutorial/server.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+from __future__ import absolute_import
+
+import os
+import json
+import time
+
+from flask import Flask, Response, request
+
+
+app = Flask(__name__, static_url_path='', static_folder='public')
+app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
+
+@app.route('/api/comments', methods=['GET', 'POST'])
+def comments_handler():
+
+    with open('comments.json', 'r') as f:
+        comments = json.loads(f.read())
+
+    if request.method == 'POST':
+        newComment = request.form.to_dict()
+        newComment['id'] = int(time.time() * 1000)
+        comments.append(newComment)
+
+        with open('comments.json', 'w') as f:
+            file.write(json.dumps(comments, indent=4, separators=(',', ': ')))
+
+    return Response(json.dumps(comments), mimetype='application/json', headers={'Cache-Control': 'no-cache'})
+
+
+if __name__ == "__main__":
+    app.run(port=int(os.environ.get('PORT', 8000)))
